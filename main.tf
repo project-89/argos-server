31ae66f06f1a17ce5169a3c935f5da82335eab9c
Jakob <haruhunab1320@gmail.com>
2024-12-19 10:41:17 -0800

    cleanup terraform script

--- main.tf
+++ main.tf
@@ -4,35 +4,34 @@ provider "google" {
   credentials = file(var.service_account_key)
 }
 
-# Enable required Google Cloud APIs
-resource "google_project_service" "pubsub" {
-  service = "pubsub.googleapis.com"
-  disable_dependent_services = false
-  disable_on_destroy = true
-}
-
-resource "google_project_service" "scheduler" {
-  service = "cloudscheduler.googleapis.com"
-  disable_dependent_services = false
-  disable_on_destroy = true
-}
-
-# Initialize Firestore (this also sets it to Native mode)
+# Initialize Firestore
 resource "google_firestore_database" "argos_firestore_main" {
   name        = "(default)"
   location_id = var.region
   type        = "FIRESTORE_NATIVE"
+
+  # Prevent recreation
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 # Cloud Storage bucket for function source code
 resource "google_storage_bucket" "functions_bucket" {
   name     = "${var.project_id}-functions"
   location = "US"
+
+  # Prevent recreation
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
+# Prepare functions source code
 resource "null_resource" "prepare_functions" {
   triggers = {
-    source_code_hash = sha256(join("", [for f in fileset(path.module, "functions/*.js") : filesha256(f)]))
+    # Only trigger on actual code changes
+    source_code_hash = sha256(join("", [for f in fileset(path.module, "functions/src/**/*.ts") : filesha256(f)]))
   }
 
   provisioner "local-exec" {
@@ -40,106 +39,59 @@ resource "null_resource" "prepare_functions" {
   }
 }
 
-resource "google_storage_bucket_object" "functions_zip" {
-  name   = "functions.zip"
+# Upload function source to bucket
+resource "google_storage_bucket_object" "functions_source" {
+  name   = "functions-source.zip"  # Fixed name to prevent unnecessary recreation
   bucket = google_storage_bucket.functions_bucket.name
   source = "./functions.zip"
+
+  # Only update if content changes
+  content_type = "application/zip"
+  metadata = {
+    hash = sha256(filebase64("./functions.zip"))
+  }
 
   depends_on = [null_resource.prepare_functions]
 }
 
-# Cloud Functions setup
-# Cloud Function: Log Visit
-resource "google_cloudfunctions_function" "logVisit" {
-  name        = "argos-visit-logger"
-  description = "Logs user visits to Firestore"
-  runtime     = "nodejs18"
-  entry_point = "logVisit"
-  source_archive_bucket = google_storage_bucket.functions_bucket.name
-  source_archive_object = google_storage_bucket_object.functions_zip.name
-  trigger_http = true
+# Main API Function (v2)
+resource "google_cloudfunctions2_function" "main_api" {
+  name        = "argos-api"
+  description = "Main API function handling all endpoints"
+  location    = var.region
 
-  environment_variables = {
-    FIRESTORE_PROJECT_ID = var.project_id
+  build_config {
+    runtime     = "nodejs18"
+    entry_point = "api"
+    source {
+      storage_source {
+        bucket = google_storage_bucket.functions_bucket.name
+        object = google_storage_bucket_object.functions_source.name
+      }
+    }
+  }
+
+  service_config {
+    max_instance_count = 100
+    available_memory   = "256M"
+    timeout_seconds    = 60
+    environment_variables = {
+      FIRESTORE_PROJECT_ID = var.project_id
+      NODE_ENV = "production"
+    }
+  }
+
+  # Ignore changes that don't affect functionality
+  lifecycle {
+    ignore_changes = [
+      build_config[0].source[0].storage_source[0].generation,
+      service_config[0].service_account_email,
+      service_config[0].ingress_settings
+    ]
   }
 }
 
-# Cloud Function: Assign Role
-resource "google_cloudfunctions_function" "assignRole" {
-  name        = "argos-assign-role"
-  description = "Assigns roles to fingerprints"
-  runtime     = "nodejs18"
-  entry_point = "assignRole"
-  source_archive_bucket = google_storage_bucket.functions_bucket.name
-  source_archive_object = google_storage_bucket_object.functions_zip.name
-  trigger_http = true
-
-  environment_variables = {
-    FIRESTORE_PROJECT_ID = var.project_id
-  }
-}
-
-# Cloud Function: Get Visited Sites
-resource "google_cloudfunctions_function" "getVisitedSites" {
-  name        = "argos-get-visited-sites"
-  description = "Retrieves visited sites for a fingerprint"
-  runtime     = "nodejs18"
-  entry_point = "getVisitedSites"
-  source_archive_bucket = google_storage_bucket.functions_bucket.name
-  source_archive_object = google_storage_bucket_object.functions_zip.name
-  trigger_http = true
-
-  environment_variables = {
-    FIRESTORE_PROJECT_ID = var.project_id
-  }
-}
-
-# Cloud Function: Add or Update Tags
-resource "google_cloudfunctions_function" "addOrUpdateTags" {
-  name        = "argos-add-or-update-tags"
-  description = "Adds or updates tags for a fingerprint"
-  runtime     = "nodejs18"
-  entry_point = "addOrUpdateTags"
-  source_archive_bucket = google_storage_bucket.functions_bucket.name
-  source_archive_object = google_storage_bucket_object.functions_zip.name
-  trigger_http = true
-
-  environment_variables = {
-    FIRESTORE_PROJECT_ID = var.project_id
-  }
-}
-
-# Cloud Function: Update Roles Based on Tags
-resource "google_cloudfunctions_function" "updateRolesBasedOnTags" {
-  name        = "argos-update-roles-based-on-tags"
-  description = "Updates roles based on tags for a fingerprint"
-  runtime     = "nodejs18"
-  entry_point = "updateRolesBasedOnTags"
-  source_archive_bucket = google_storage_bucket.functions_bucket.name
-  source_archive_object = google_storage_bucket_object.functions_zip.name
-  trigger_http = true
-
-  environment_variables = {
-    FIRESTORE_PROJECT_ID = var.project_id
-  }
-}
-
-# Cloud Function: Get Available Roles
-resource "google_cloudfunctions_function" "getAvailableRoles" {
-  name        = "argos-get-available-roles"
-  description = "Retrieves available roles"
-  runtime     = "nodejs18"
-  entry_point = "getAvailableRoles"
-  source_archive_bucket = google_storage_bucket.functions_bucket.name
-  source_archive_object = google_storage_bucket_object.functions_zip.name
-  trigger_http = true
-
-  environment_variables = {
-    FIRESTORE_PROJECT_ID = var.project_id
-  }
-}
-
-# Cloud Function: Scheduled Cleanup
+# Scheduled Cleanup Function (v2)
 resource "google_cloudfunctions2_function" "scheduledCleanup" {
   name        = "argos-scheduled-cleanup"
   description = "Performs scheduled cleanup of old data"
@@ -165,14 +117,38 @@ resource "google_cloudfunctions2_function" "scheduledCleanup" {
       NODE_ENV = "production"
     }
   }
+
+  # Ignore changes that don't affect functionality
+  lifecycle {
+    ignore_changes = [
+      build_config[0].source[0].storage_source[0].generation,
+      service_config[0].service_account_email,
+      service_config[0].ingress_settings
+    ]
+  }
 }
 
-# Upload the source code directory for v2 function
-resource "google_storage_bucket_object" "functions_source" {
-  name   = "functions-source-${formatdate("YYYYMMDDhhmmss", timestamp())}.zip"
-  bucket = google_storage_bucket.functions_bucket.name
-  source = "./functions.zip"
+# Cloud Scheduler job
+resource "google_cloud_scheduler_job" "cleanup" {
+  name             = "argos-cleanup"
+  description      = "Triggers cleanup function daily"
+  schedule         = "0 0 * * *"  # Run at midnight every day
+  time_zone        = "UTC"
+  region           = var.region
 
-  depends_on = [null_resource.prepare_functions]
+  http_target {
+    http_method = "POST"
+    uri         = google_cloudfunctions2_function.scheduledCleanup.url
+    oidc_token {
+      service_account_email = "${var.project_id}@appspot.gserviceaccount.com"
+    }
+  }
+
+  # Ignore changes to service account and other metadata
+  lifecycle {
+    ignore_changes = [
+      http_target[0].oidc_token[0].service_account_email
+    ]
+  }
 }
 
