0d6d2fda154c4bca07fd68c91a40c999dbe78a05
Jakob <haruhunab1320@gmail.com>
2025-01-16 13:24:12 -0800

    remove uneeded type

--- functions/src/types/module-alias.d.ts
+++ functions/src/types/module-alias.d.ts
@@ -1,5 +1,5 @@
 declare module "module-alias" {
-  export function addAliases(aliases: { [key: string]: string }): void;
+  export function addAliases(aliases: Record<string, string>): void;
   export function addPath(path: string): void;
   export function isPathMatchesAlias(path: string, alias: string): boolean;
   export function reset(): void;
