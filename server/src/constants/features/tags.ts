00c08066d2a2becdf4850c2d8181033392fd35c3
Jakob <haruhunab1320@gmail.com>
2025-02-13 09:34:37 -0800

    add tag limit const

--- server/src/constants/features/tags.ts
+++ server/src/constants/features/tags.ts
@@ -3,3 +3,7 @@ export const ALLOWED_TAG_TYPES = {
 } as const;
 
 export type TagType = (typeof ALLOWED_TAG_TYPES)[keyof typeof ALLOWED_TAG_TYPES];
+
+export const TAG_LIMITS = {
+  DAILY_TAGS: 3,
+} as const;
